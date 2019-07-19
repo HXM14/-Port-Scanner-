907ba35ae90144aebacb4037ca13fe7f55a5f694
hazem <36288517+hxazem@users.noreply.github.com>
2019-07-19 19:05:51 +0200

    Update Port_scanner.py

--- Port_scanner.py
+++ Port_scanner.py
@@ -1,16 +1,15 @@
 import socket
 
-Host_name = str(input("Enter Host Name : "))
-Host_ipv4 = socket.gethostbyname(Host_name)
+Host_name = str(input("Enter Host Name : ")) #Enter Host Address i.e www.facebook.com
+Host_ipv4 = socket.gethostbyname(Host_name) 
 print("Host IPV4 {0}".format(Host_ipv4))
 
 option = True
 while option:
-    port = int(input("port : "))
+    port = int(input("port : ")) #Port To Scan 
     try:
         soc_object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        is_open = soc_object.connect((Host_name, port))
-        print(is_open)
+        soc_object.connect((Host_name, port))
         print("Port {}: Open".format(port))
         soc_object.close()
     except:
